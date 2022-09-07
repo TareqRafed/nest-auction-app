1cc8d0964e8fe823a07fd91380712c4461770356
TareqRafed <tareq.rafed22@gmail.com>
2022-09-07 08:11:54 +0300

    feat: integrate prisma, setup env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    // env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // modules
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.server';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDTO } from './dto';
+import { SignInDTO } from './dto/signin.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: SignUpDTO) {
+    console.log(dto);
     return this.authService.signup();
   }
 
   @Post('signin')
-  signin() {
+  signin(@Body() dto: SignInDTO) {
+    console.log(dto);
     return this.authService.signin();
   }
 }
